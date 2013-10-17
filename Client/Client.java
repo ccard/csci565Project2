27341102aaf7764a03b667dfab4de6e03ad43110
Chris Card <ccard@mymail.mines.edu>
2013-10-17 17:45:48 -0600

    Removed old code

--- Client/Client.java
+++ Client/Client.java
@@ -14,39 +14,6 @@ import java.io.*;
 
 public class Client
 {
-	/**
-	* This reades from the file passed to the client and creates a string representing that message
-	* @param String filename to read from
-	* @return String that represents the message in the text file
-	*/
-	public static String makeMessageText(String file)
-	{
-		//String builder reduces the number of concatinations needed to form a long string from
-		//multiple strings
-		StringBuilder message = new StringBuilder();
-
-		if(file.isEmpty())
-		{
-			return "";
-		}
-		try{
-			//read in the message from the file
-			BufferedReader msg = new BufferedReader(new FileReader(new File(file)));
-		
-			String line = "";
-
-			while((line = msg.readLine()) != null)
-			{
-				message.append(line);
-				message.append("\n");
-			}
-		} catch (Exception e) {
-			System.err.println("Could not read message file!");
-			e.printStackTrace();
-		}
-
-		return message.toString();
-	}
 	
 	/**
 	* @param String list of arguments that must be in following order: 
@@ -88,31 +55,7 @@ public class Client
 			Messages task = new Messages(message,sendReceive);
 			Message ret = comp.sendReceive(task);
 
-			//If the it is sending to server then checks to see if the returned message is null or
-			//the same message
-			if(sendReceive.compareTo("send") == 0)
-			{
-				if(null == ret)
-				{
-					System.out.println("Message was not received or it was lost!");
-					return;
-				}
-				if(ret.getMessage().compareTo(task.getMessage()) == 0)
-				{
-					System.out.println("Message sent");
-				}
-			}
-			else
-			{
-				if(null == ret)
-				{
-					System.out.println("The Server has no messages at this time!");
-				}
-				else
-				{
-					System.out.println(ret.getMessage());
-				}
-			}
+			
 		}catch(Exception e){
 			System.err.println("Client exception:");
 			e.printStackTrace();
