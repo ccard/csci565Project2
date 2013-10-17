b5ff448c7468fc28a799a44687d7fe3f147b9b2b
Chris Card <ccard@mymail.mines.edu>
2013-10-17 17:46:15 -0600

    implemented the article class

--- Compute/Article.java
+++ Compute/Article.java
@@ -5,9 +5,33 @@
 
 package Compute;
 
-public interface Message
+public class Article implements Serializable
 {
-		  String getMessage();
+	private static final long serialVersionUID = 227L;
 
-		  String getMsgType();
+	public final String content;
+	public final int id;
+	public final int parent;
+
+	/**
+	* Constructor
+	* @param msg message to store
+	* @param type the type of action to take when sent to the server, either send or receive
+	*/
+	public Article(String body, int parent);
+	{
+		this(body,-1,parent);
+	}
+
+	private Article(String body, int id, int parent)
+	{
+		content = body;
+		this.id = id;
+		this.parent = parent;
+	}
+
+	public Article setId(int id)	
+	{
+		return new Article(this.content,id,this.parent);
+	}
 }
