540cd673078d689e24d5cfcd299445ee04369d86
Steven Ruppert <strupper@mines.edu>
2013-10-22 18:05:39 -0600

    Import @GetGeneratedKeys

--- Server/ArticleStore.java
+++ Server/ArticleStore.java
@@ -2,10 +2,7 @@ package Server;
 
 import Compute.Article;
 import org.skife.jdbi.v2.StatementContext;
-import org.skife.jdbi.v2.sqlobject.Bind;
-import org.skife.jdbi.v2.sqlobject.BindBean;
-import org.skife.jdbi.v2.sqlobject.SqlQuery;
-import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.*;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 import org.skife.jdbi.v2.tweak.ResultSetMapper;
 
