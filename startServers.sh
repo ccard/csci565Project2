f8d2b63956592362b4f58bc37c0deaa89927775a
Chris Card <ccard@mymail.mines.edu>
2013-10-24 18:19:31 -0600

    is now working

--- startServers.sh
+++ startServers.sh
@@ -1,17 +1,27 @@
 #! /usr/bin/env bash
 
 master=""
-if []
-while read x; do
- type=`echo $x | cut --delimiter=':' -f 1 -`
- host=`echo $x | cut --delimiter=':' -f 3 -`
- port=`echo $x | cut --delimiter=':' -f 5 -`
- path=`pwd`
- if [ $type = 'master' ]; then
- master="$host:$port"
-    echo `ssh $host "cd $path; ./runServer.sh -s $port -master"`
- else
-	echo `ssh $host "cd $path; ./runServer.sh -s $port -slave -mhost $master"`
- fi
-
-done <hosts.txt
+if [ $# = 1 ]; then
+	if [ $1 = '-start' ]; then
+		while read x; do
+ 			type=`echo $x | cut --delimiter=':' -f 1 -`
+ 			host=`echo $x | cut --delimiter=':' -f 3 -`
+ 			port=`echo $x | cut --delimiter=':' -f 5 -`
+ 			path=`pwd`
+ 			if [ $type = 'master' ]; then
+ 				master="$host:$port"
+    			echo `ssh $host "cd $path; ./runServer.sh -s $port -master"`
+ 			else
+				echo `ssh $host "cd $path; ./runServer.sh -s $port -slave -mhost $master"`
+ 			fi
+		done <hosts.txt
+	else
+		while read x; do
+ 			host=`echo $x | cut --delimiter=':' -f 3 -`
+    		echo `ssh $host "pkill java"`
+		done <hosts.txt
+	fi
+else
+	echo "Usage: ./startServers.sh [-start | -stop]"
+	echo " the hosts.txt file must exists in this directory"
+fi
