c9c51f06a684516d73475adc81458f2f7bdcb830
Chris Card <ccard@mymail.mines.edu>
2013-10-31 17:41:37 -0600

    updted regex

--- getStats.rb
+++ getStats.rb
@@ -15,7 +15,7 @@ end
 def readFile(file)
 	input = File.open file,'r'
 	input.each do |line|
-		if /(^.+<)(LIST|POST|CHOOSE)(>.+)(\d+ | \d+\.\d+)(\s+)(ms$)/ =~ line
+		if /(^.+<)(LIST|POST|CHOOSE)(>.+)(\d+ | \d+\.\d+)(\s+)(ms .*$)/ =~ line
 			writeFile "#{$2},#{$4},#{$6}"
 		end
 	end
